2e56a5131100f885b2c06b92941219d72a67d99f
rabbiafatima850 <156567170+rabbiafatima850@users.noreply.github.com>
2024-11-07 17:14:30 +0500

    updated assign 2

---
+++ assign 2.py
@@ -0,0 +1,41 @@
+healthcare_diagnostics=input('enter your problem :')
+
+
+if healthcare_diagnostics!='':
+     
+#for headache.
+    if healthcare_diagnostics=='headache':
+     print ('flu')
+#for eye problem.
+    elif healthcare_diagnostics=='eye burning':
+     print ('eye infection')
+#for chest pain.
+    elif healthcare_diagnostics  =='chest pain':
+     print( 'angina')
+#for throat problem.
+    elif healthcare_diagnostics=='throat':
+     print('tonsils')
+#for lung infection.
+    elif healthcare_diagnostics=='lung infection':
+     print('pneumonia')
+#for acidity.
+    elif healthcare_diagnostics=='acidity':
+     print('stomach-problem')
+#for diarrhea.
+    elif healthcare_diagnostics=='diarrhea':
+     print('infection or wound in intestine')
+    else:
+     print('discuss with your consultant')
+elif healthcare_diagnostics=='':
+     print('i have no idea')
+else:
+     print('plz mention your problem')
+
+print(healthcare_diagnostics) 
+
+
+
+
+#prepared by:rabbia fatima, ghazala and maira.
+
+  
